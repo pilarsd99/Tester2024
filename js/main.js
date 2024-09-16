d6faf412cdc0474b7b2b9b4a4ee5ec828d5a2cee
Pilar <pilarsd99@gmail.com>
2024-09-16 10:52:44 -0300

    usando metodos

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 let nombre ="Pilar";
-let apellido = 'Sahonero';
-let quienSos =`Bar \"la tortuguita\"`;
+let apellido = 'SAHONERO';
+let quienSos ="Bar \"la tortuguita\"";
 
 //quienSos = nombre+' '+apellido;
 quienSos = `${nombre} ${apellido}`;
 alert(nombre);
 alert(apellido);
 alert(quienSos);
+alert(nombre.length);
+alert(nombre.toUpperCase());
+alert(apellido.toLowerCase());
+alert(nombre.replace('Pilar', 'Pablo'))
